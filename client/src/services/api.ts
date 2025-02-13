03dd519daf6dedf12c3f45e850aa92b9a6257f2b
lazar15x <166030992+lazar15x@users.noreply.github.com>
2025-02-13 21:05:19 +0300

    add logout, refactoring, upd. profile page

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -14,6 +14,7 @@ api.interceptors.request.use(async config => {
   if (accessToken) {
     config.headers['Authorization'] = `Bearer ${accessToken}`;
   }
+
   return config;
 });
 
--- client/src/stores/authStore.ts
+++ client/src/stores/authStore.ts
@@ -2,7 +2,7 @@ import { makeAutoObservable } from 'mobx';
 import * as AuthService from '../services/AuthService';
 
 class AuthStore {
-  accessToken: string | null = '';
+  accessToken: string | null = null;
   status: 'idle' | 'loading' | 'success' | 'failed' = 'idle';
   error: string | null = null;
 
@@ -12,56 +12,57 @@ class AuthStore {
   }
 
   async register(email: string, username: string, password: string) {
-    this.setLoading()
+    this.setLoading();
 
     try {
       const response = await AuthService.register(email, username, password);
-      this.setToken(response.data.access_token)
+      this.setToken(response.data.access_token);
     } catch (err: any) {
-      this.setError(err.message)
+      this.setError(err.message);
     }
   }
 
   async login(email: string, password: string) {
-    this.status = 'loading';
-    this.error = null;
+    this.setLoading();
 
     try {
       const response = await AuthService.login(email, password);
-      this.setToken(response.data.access_token)
+      this.setToken(response.data.access_token);
     } catch (err: any) {
-      this.setError(err.message)
+      this.setError(err.message);
     }
   }
 
   async refreshToken() {
-    this.status = 'loading';
-    this.error = null;
+    this.setLoading();
+
     try {
       const response = await AuthService.refreshToken();
-      this.setToken(response.data.access_token)
+      this.setToken(response.data.access_token);
+      console.log('New access token:', this.accessToken);
     } catch (err: any) {
-      this.setError(err.message)
+      this.setError(err.message);
     }
   }
 
   async logout() {
-    this.setToken(null)
+    this.setToken(null);
+    await AuthService.logout();
   }
 
   private setLoading() {
-    this.status = 'loading'
-    this.error = null
+    this.error = null;
+    this.status = 'loading';
   }
 
   private setToken(token: string | null) {
-    this.accessToken = token
-    this.status = 'success'
+    this.accessToken = token;
+    this.status = 'success';
   }
 
   private setError(error: string) {
-    this.error = error
-    this.status = 'failed'
+    this.error = error;
+    this.status = 'failed';
   }
 }
 
